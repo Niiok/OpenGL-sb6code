c305913663330d23629a815407dd6a87447d7fde
Niiok <play.niiok@gmail.com>
2021-02-05 23:17:40 +0900

    gitignore error

--- AOpenGL/vertex.h
+++ AOpenGL/vertex.h
@@ -6,7 +6,7 @@ static const GLchar* vertex_shader_source[] =
 	"																				\n"
 	"	void main(void)																\n"
 	"	{																			\n"
-	"		const vec4 vertices[3] = vec4[3] (vec4(0.25, -0.25, 0.5, 1.0), 			\n"
+	"		const vec4 vertices[3] = vec4[3](vec4(0.25, -0.25, 0.5, 1.0),			\n"
 	"										 vec4(-0.25, -0.25, 0.5, 1.0),			\n"
 	"										 vec4(0.25, 0.25, 0.5, 1.0));			\n"
 	"																				\n"
